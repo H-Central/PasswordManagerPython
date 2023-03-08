56e3c512927bb37efb0ee844e4950a157c75e5f4
Zerbaib <hugo.hennetin@proton.me>
2023-03-08 07:06:41 +0100

    add selection

--- helpers/gen.py
+++ helpers/gen.py
@@ -2,3 +2,19 @@ lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', '
 higer = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 special = [ '%' , '_' , '-' , '!' , '$' , '^' , '&' , '#' , '(' , ')' , '[' , ']' , '=' , '@']
+
+def pwd(n , low = True, hig = True, num = True, spe = True):
+    alphabets = dict()
+    key = 0
+    if low:
+        alphabets[key] = lower
+        key += 1
+    if hig:
+        alphabets[key] = higer
+        key += 1
+    if num:
+        alphabets[key] = number
+        key += 1
+    if spe:
+        alphabets[key] = special
+        key += 1
